20bf9233d93c977ea97ea78973a613b426ece40a
Sangram Das <sangramraju143@gmail.com>
2025-07-17 17:31:46 +0530

    Add navbar in it

---
+++ src/Components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Home.css"
+
+import { Terminal, Github , Twitter } from "lucide-react";
+
+const Home = () => {
+  return (
+    <div className='back-div'>
+        <div className='left'>
+        <p>Sangram </p>
+        <span><Terminal className="ml-3 w-6 h-6 text-green-500" /></span>
+
+        </div>
+        
+      <nav>
+        <a>Home</a>
+        <a>About</a>
+        <a>Project</a>
+        <a>Contact-us</a>
+      </nav>
+      <div className='social-icon'>
+         <Github className="ml-3 w-6 h-6 text-green-500" />
+      <Twitter className="ml-3  w-6 h-6 text-red-600" />
+      </div>
+    </div>
+  )
+}
+
+export default Home
